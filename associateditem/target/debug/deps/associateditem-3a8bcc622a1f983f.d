5f670efa5ec9bf3c9822e3196203a621a3d3f653
jannes-sa <jannes.santoso@tiket.com>
2020-01-03 18:35:48 +0700

    add phantom

--- associateditem/target/debug/deps/associateditem-3a8bcc622a1f983f.d
+++ associateditem/target/debug/deps/associateditem-3a8bcc622a1f983f.d
@@ -1,6 +1,6 @@
-/home/jannes/work/rust/test1/associateditem/target/debug/deps/associateditem-3a8bcc622a1f983f.rmeta: src/main.rs src/ass1.rs
+/home/jannes/work/rust/rust_test1/associateditem/target/debug/deps/associateditem-3a8bcc622a1f983f.rmeta: src/main.rs src/ass1.rs
 
-/home/jannes/work/rust/test1/associateditem/target/debug/deps/associateditem-3a8bcc622a1f983f.d: src/main.rs src/ass1.rs
+/home/jannes/work/rust/rust_test1/associateditem/target/debug/deps/associateditem-3a8bcc622a1f983f.d: src/main.rs src/ass1.rs
 
 src/main.rs:
 src/ass1.rs:
--- associateditem/target/debug/deps/associateditem-e0869142e1a0212a.d
+++ associateditem/target/debug/deps/associateditem-e0869142e1a0212a.d
@@ -1,6 +1,6 @@
-/home/jannes/work/rust/test1/associateditem/target/debug/deps/associateditem-e0869142e1a0212a.rmeta: src/main.rs src/ass1.rs
+/home/jannes/work/rust/rust_test1/associateditem/target/debug/deps/associateditem-e0869142e1a0212a.rmeta: src/main.rs src/ass1.rs
 
-/home/jannes/work/rust/test1/associateditem/target/debug/deps/associateditem-e0869142e1a0212a.d: src/main.rs src/ass1.rs
+/home/jannes/work/rust/rust_test1/associateditem/target/debug/deps/associateditem-e0869142e1a0212a.d: src/main.rs src/ass1.rs
 
 src/main.rs:
 src/ass1.rs:
